6229e6564e42b2bbedbd149a61104c9f492299ff
Galkina-Darya <99248872+Galkina-Darya@users.noreply.github.com>
2022-03-20 18:12:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,49 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int n = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j=0; j < i + 1; j++) {
+      if (*(arr + i) + *(arr + j)) == value)
+        n++;
+    }
+  }
+  return n;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int n = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = 0; j > i; j--) {
+      if (*(arr + i)) + *(arr + j)) == value)
+        n++;
+    }
+  }
+  return n;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int n = 0;
+  for (int i = 0; i < len - 1; i++) {
+    int left = i, right = len;
+    while (left < right - 1) {
+      int mid = (left + right) / 2;
+      if (*(arr + i) + *(arr + mid)) == value) {
+        n++;
+        int j = mid + 1;
+        while (*(arr + i) + *(arr + j)) == value && j < right) {
+          n++;
+          n++;
+        }
+        j = mid - 1;
+        while (*(arr + i) + *(arr + j)) == value && j > right) {
+          n++;
+          j--;
+        }
+        break;
+      }
+      if *(arr + i) + *(arr + mid) > value)
+        right = mid;
+      else
+        left = mid;
+    }
+  }
+  return n;
 }
