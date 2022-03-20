22f561de674112db8a366004518b60af5a1209cf
Galkina-Darya <99248872+Galkina-Darya@users.noreply.github.com>
2022-03-20 18:19:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@ int countPairs1(int *arr, int len, int value) {
   int n = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j=0; j < i + 1; j++) {
-      if (*(arr + i) + *(arr + j)) == value)
+      if ((*(arr + i) + *(arr + j)) == value)
         n++;
     }
   }
@@ -13,7 +13,7 @@ int countPairs2(int *arr, int len, int value) {
   int n = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = 0; j > i; j--) {
-      if (*(arr + i)) + *(arr + j)) == value)
+      if (((*(arr + i)) + (*(arr + j))) == value)
         n++;
     }
   }
@@ -25,7 +25,7 @@ int countPairs3(int *arr, int len, int value) {
     int left = i, right = len;
     while (left < right - 1) {
       int mid = (left + right) / 2;
-      if (*(arr + i) + *(arr + mid)) == value) {
+      if ((*(arr + i) + *(arr + mid)) == value) {
         n++;
         int j = mid + 1;
         while (*(arr + i) + *(arr + j)) == value && j < right) {
@@ -39,8 +39,9 @@ int countPairs3(int *arr, int len, int value) {
         }
         break;
       }
-      if *(arr + i) + *(arr + mid) > value)
+      if *(arr + i) + *(arr + mid) > value) {
         right = mid;
+      }
       else
         left = mid;
     }
